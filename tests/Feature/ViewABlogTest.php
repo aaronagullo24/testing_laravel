80ae36d48490b102d5fe564148904651c75477b5
aaron agullo sanchez <aaronagullo24@gmail.com>
2020-04-04 18:48:57 +0200

    Running test to green

--- tests/Feature/ViewABlogTest.php
+++ tests/Feature/ViewABlogTest.php
@@ -2,18 +2,35 @@
 
 namespace Tests\Feature;
 
+use App\Post;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\Artisan;
 
 class ViewABlogTest extends TestCase
 {
 
-    public function testCanViewABlogPost(){
-        //Arrangement
+    use DatabaseMigrations;
 
+    public function testCanViewABlogPost(){
         
+        $post = Post::create([
+            'title'=>'A simple title',
+            'body'=>'A simple body',
+        ]);
+
+        $resp = $this->get("/post/{$post->id}");
+
+        $resp->assertStatus(200);
+
+        $resp->assertSee($post->title);
+
+        $resp->assertSee($post->body);
+
+        $resp->assertSee($post->created_at);
+
     }
 
 }
